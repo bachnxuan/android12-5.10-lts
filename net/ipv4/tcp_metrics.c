b3864fd02075b6284c3c9f60bf20b01d03ccf6fc
bachnxuan <bachnxuan@gmail.com>
2025-05-23 22:14:06 +0700

    squash: Revert all BBR commits
    Revert "net-tcp_bbr: v3: merge changes from bbr-v3-2024-02-22-01"
    
    This reverts commit 8f1e95a3b7c26141affb3f5d4cc4547f6877de14.
    
    Revert "tcp: export TCPI_OPT_ECN_LOW in tcp_info tcpi_options field"
    
    This reverts commit ffeb0419fc701208327d3233ce6eac8c9f47ae09.
    
    Revert "net-tcp_bbr: v3: ensure ECN-enabled BBR flows set ECT on retransmits"
    
    This reverts commit e5bb66561851911ea2182b9a583fe9b4c3580f80.
    
    Revert "net-tcp_bbr: introduce bbr_param_enabled macro for boolean check"
    
    This reverts commit 7130b81410e2860931654d845b117a98ea8fd143.
    
    Revert "net-tcp_bbr: v3: update TCP "bbr" congestion control module to BBRv3"
    
    This reverts commit a4f1d2f1e8f93756af748290123e4b829e0ae218.
    
    Revert "tcp: introduce per-route feature RTAX_FEATURE_ECN_LOW"
    
    This reverts commit dfdcd4aa8d493b8816e745d66730f9c50c1605e0.
    
    Revert "net-tcp_bbr: v2: introduce is_acking_tlp_retrans_seq into rate_sample"
    
    This reverts commit 195d1d9e70473ef12d16298a94d72adc4aaf4b53.
    
    Revert "net-tcp_bbr: v2: inform CC module of losses repaired by TLP probe"
    
    This reverts commit d0aeac31a058db2c7f7c090d1b9ec6f13426956e.
    
    Revert "net-tcp_bbr: v2: record app-limited status of TLP-repaired flight"
    
    This reverts commit db5e738dfb3196f1a5d8465ab99277f614dc98c8.
    
    Revert "net-tcp: add fast_ack_mode=1: skip rwin check in tcp_fast_ack_mode__tcp_ack_snd_check()"
    
    This reverts commit c0ddb9cd719e611c33bb85e79d81699d9601644f.
    
    Revert "net-tcp: re-generalize TSO sizing in TCP CC module API"
    
    This reverts commit 1def1a68a4c979fc538d4c481ee0bf7f94059fa6.
    
    Revert "net-tcp: add new ca opts flag TCP_CONG_WANTS_CE_EVENTS"
    
    This reverts commit eb5ebfa364e748edf8180c2047693fc0b6b3ccb6.
    
    Revert "net-tcp_bbr: v2: adjust skb tx.in_flight upon split in tcp_fragment()"
    
    This reverts commit 1b4fafb5eb51aeacc9fa1d52b5b56432d80eb7c1.
    
    Revert "net-tcp_bbr: v2: adjust skb tx.in_flight upon merge in tcp_shifted_skb()"
    
    This reverts commit 8845b93f3fc51dc8561c75e11fcefa06eee4be5d.
    
    Revert "net-tcp_bbr: v2: introduce ca_ops->skb_marked_lost() CC module callback API"
    
    This reverts commit c836f0727c155b797081b44bc23316cdf8356556.
    
    Revert "net-tcp_bbr: v2: export FLAG_ECE in rate_sample.is_ece"
    
    This reverts commit 1e0d66eb4ba27aa24e79315a0daeda83931320ba.
    
    Revert "net-tcp_bbr: v2: count packets lost over TCP rate sampling interval"
    
    This reverts commit 9f8bfc6f4ed355d9bb162ccd446b0812201faff5.
    
    Revert "net-tcp_bbr: v2: snapshot packets in flight at transmit time and pass in rate_sample"
    
    This reverts commit 219a11ffe819c192077e6681be7bda5e93352456.
    
    Revert "net-tcp_bbr: v2: shrink delivered_mstamp, first_tx_mstamp to u32 to free up 8 bytes"
    
    This reverts commit 34f599b4e21e480bbaedd04006e250b4921cfb55.
    
    Revert "net-tcp_bbr: broaden app-limited rate sample detection"
    
    This reverts commit e50b4567bdfe3f5c5fe1e2bacd70e83d67ea971d.
    
    Revert "tcp: tracking packets with CE marks in BW rate sample"
    
    This reverts commit 17571ddf03c3da30409ec11be4a481250ebb0a0a.
    
    Revert "tcp: add accessors to read/set tp->snd_cwnd"
    
    This reverts commit 782dc3ab2549f067915c1084d919b162904fd1f3.
    
    Revert "tcp: add rcv_wnd and plb_rehash to TCP_INFO"
    
    This reverts commit 98034fbf1861edb83eb1356e7bf9a56a0bcce4c4.
    
    Revert "tcp: add u32 counter in tcp_sock and an SNMP counter for PLB"
    
    This reverts commit 9d5794850a4e5f9e3449b5b81c212b4d1a97218b.
    
    Revert "tcp: add support for PLB in DCTCP"
    
    This reverts commit e95ea3acddeebe6b9bd86216aa3e6e8dfa60941f.
    
    Revert "tcp: add PLB functionality for TCP"
    
    This reverts commit f7bc9b06102e94d673161809c6fc5b17594766a4.
    
    Revert "tcp: add sysctls for TCP PLB parameters"
    
    This reverts commit 679b9069d125b46b920a8fda670b9cfbcd152adb.
    
    Signed-off-by: bachnxuan <bachnxuan@gmail.com>

--- net/ipv4/tcp_metrics.c
+++ net/ipv4/tcp_metrics.c
@@ -408,15 +408,15 @@ void tcp_update_metrics(struct sock *sk)
 		if (!READ_ONCE(net->ipv4.sysctl_tcp_no_ssthresh_metrics_save) &&
 		    !tcp_metric_locked(tm, TCP_METRIC_SSTHRESH)) {
 			val = tcp_metric_get(tm, TCP_METRIC_SSTHRESH);
-			if (val && (tcp_snd_cwnd(tp) >> 1) > val)
+			if (val && (tp->snd_cwnd >> 1) > val)
 				tcp_metric_set(tm, TCP_METRIC_SSTHRESH,
-					       tcp_snd_cwnd(tp) >> 1);
+					       tp->snd_cwnd >> 1);
 		}
 		if (!tcp_metric_locked(tm, TCP_METRIC_CWND)) {
 			val = tcp_metric_get(tm, TCP_METRIC_CWND);
-			if (tcp_snd_cwnd(tp) > val)
+			if (tp->snd_cwnd > val)
 				tcp_metric_set(tm, TCP_METRIC_CWND,
-					       tcp_snd_cwnd(tp));
+					       tp->snd_cwnd);
 		}
 	} else if (!tcp_in_slow_start(tp) &&
 		   icsk->icsk_ca_state == TCP_CA_Open) {
@@ -424,10 +424,10 @@ void tcp_update_metrics(struct sock *sk)
 		if (!READ_ONCE(net->ipv4.sysctl_tcp_no_ssthresh_metrics_save) &&
 		    !tcp_metric_locked(tm, TCP_METRIC_SSTHRESH))
 			tcp_metric_set(tm, TCP_METRIC_SSTHRESH,
-				       max(tcp_snd_cwnd(tp) >> 1, tp->snd_ssthresh));
+				       max(tp->snd_cwnd >> 1, tp->snd_ssthresh));
 		if (!tcp_metric_locked(tm, TCP_METRIC_CWND)) {
 			val = tcp_metric_get(tm, TCP_METRIC_CWND);
-			tcp_metric_set(tm, TCP_METRIC_CWND, (val + tcp_snd_cwnd(tp)) >> 1);
+			tcp_metric_set(tm, TCP_METRIC_CWND, (val + tp->snd_cwnd) >> 1);
 		}
 	} else {
 		/* Else slow start did not finish, cwnd is non-sense,
